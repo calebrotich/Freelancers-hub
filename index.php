68cadfc3464dd857250c47c2663e8b2071352c00
marler1 <edetricky@gmail.com>
2018-06-15 19:42:04 +0300

    initial

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>index</title>
+    <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+           <span class="sr-only">Toggle navigation</span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+          </button>
+        <a class="navbar-brand" href="#">Freelancers-hub</a>
+      </div>
+      <!--<div class="collapse-collapse">-->
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="nav navbar-nav">
+          <li><a href="trees.php"><span class="glyphicon glyphicon-home"></span>HOME</a></li></ul>
+        <ul class="nav navbar-nav navbar-right">
+           <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span>POST JOB</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span>HIRE FREELANCER</a></li>
+           <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span>LOG IN</a></li>
+          <li><a href="registration.php"><span class="glyphicon glyphicon-pencil"></span>SIGNUP</a></li>
+        </ul>
+       </ul>
+      </div>
+    </div>
+  </nav>
+
+  </head>
+  <body>
+
+  </body>
+</html>
